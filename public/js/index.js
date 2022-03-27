522d123f03df16ae6cedeb9f26098926239ac932
nam17081999 <nam17081999@gmail.com>
2022-03-27 20:00:54 +0700

    add play, prev

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,30 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
+const heading = $('header h2')
+const cdThumb = $('.cd-thumb')
+const audio = $('#audio')
+const cd = $('.cd')
+const playBtn = $('.btn-toggle-play')
+const player = $('.player')
+const progress = $('#progress')
+const nextBtn = $('.btn-next')
+const prevBtn = $('.btn-prev')
 
 const app = {
+    currentIndex: 0,
+    isPlaying: false,
+
     songs: [
         {
-            name: "Click Pow Get Down",
-            singer: "Raftaar x Fortnite",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=34737&format=320",
-            image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
+            name: "Damn",
+            singer: "Raftaar x kr$na",
+            path:
+                "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
+            image:
+                "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg"
         },
         {
+
             name: "Tu Phir Se Aana",
             singer: "Raftaar x Salim Merchant x Karma",
             path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
@@ -17,12 +32,13 @@ const app = {
                 "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
         },
         {
-            name: "Naachne Ka Shaunq",
-            singer: "Raftaar x Brobha V",
-            path:
-                "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
-            image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
+            name: "Click Pow Get Down",
+            singer: "Raftaar x Fortnite",
+            path: "https://mp3.vlcmusic.com/download.php?track_id=34737&format=320",
+            image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
         },
+
+
         {
             name: "Mantoiyat",
             singer: "Raftaar x Nawazuddin Siddiqui",
@@ -37,14 +53,7 @@ const app = {
             image:
                 "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
         },
-        {
-            name: "Damn",
-            singer: "Raftaar x kr$na",
-            path:
-                "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
-            image:
-                "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg"
-        },
+
         {
             name: "Feeling You",
             singer: "Raftaar x Harjas",
@@ -70,16 +79,25 @@ const app = {
                 </div>
             </div>
             `
-
-
         })
         $('.playlist').innerHTML = htmls.join(' ')
     },
 
+    defineProperties: function () {
+        Object.defineProperty(this, 'currentSong', {
+            get: function () {
+                return this.songs[this.currentIndex]
+            }
+        })
+    },
+
+
+
     handleEvents: function () {
-        const cd = $('.cd')
+        const _this = this
         const cdWidth = cd.offsetWidth
 
+        //SỬ LÍ THU PHÓNG
         document.onscroll = function () {
             const scrollY = window.scrollY
             const newCdWidth = cdWidth - scrollY
@@ -87,10 +105,88 @@ const app = {
             cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0
             cd.style.opacity = newCdWidth / cdWidth
         }
+
+        //xu li khi click play
+        playBtn.onclick = function () {
+            if (_this.isPlaying) {
+                audio.pause()
+            } else {
+                audio.play()
+            }
+        }
+
+
+        //khi song pkay
+        audio.onplay = function () {
+            _this.isPlaying = true
+            player.classList.add('playing')
+        }
+
+        audio.onpause = function () {
+            _this.isPlaying = false
+            player.classList.remove('playing')
+        }
+
+        //Khi tiến độ bài hát thay đổi
+        audio.ontimeupdate = function () {
+            if (audio.duration) {
+                const progressPersent = Math.floor(audio.currentTime / audio.duration * 100)
+                progress.value = progressPersent
+            }
+        }
+
+        // xử lí khi tua song
+        progress.onchange = function (e) {
+            const seekTime = audio.duration * e.target.value / 100
+            audio.currentTime = seekTime
+        }
+
+        //Next song
+        nextBtn.onclick = function () {
+            _this.nextSong()
+            audio.play()
+        }
+
+        //Prev Song
+        prevBtn.onclick = function () {
+            _this.prevSong()
+            audio.play()
+        }
+    },
+
+    loadCurrentSong: function () {
+        heading.textContent = this.currentSong.name
+        cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`
+        audio.src = this.currentSong.path
+    },
+
+    nextSong: function () {
+        this.currentIndex++
+        if (this.currentIndex >= this.songs.length) {
+            this.currentIndex = 0
+        }
+        this.loadCurrentSong()
+    },
+
+    prevSong: function () {
+        this.currentIndex--
+        if (this.currentIndex < 0) {
+            this.currentIndex = this.songs.length - 1
+        }
+        this.loadCurrentSong()
     },
 
     start: function () {
+        //Định nghĩa các thuộc tính cho object
+        this.defineProperties()
+
+        //Lắng nghe và xử lí các sự kiện (DOM Even)
         this.handleEvents()
+
+        //Tải thông tin bài đầu tiên vào UI
+        this.loadCurrentSong()
+
+        //Render playlist
         this.render()
     }
 }
